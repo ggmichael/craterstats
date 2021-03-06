4e8b799fe8bf100b63ad0767d26313a36fd7da0c
Greg Michael <75208810+ggmichael@users.noreply.github.com>
2021-03-06 18:49:35 +0100

    moved gm to symlink

--- gm/idl/__init__.py
+++
@@ -1,5 +0,0 @@
-#  Copyright (c) 2021, Greg Michael
-#  Licensed under BSD 3-Clause License. See LICENSE.txt for details.
-
-from .where import where
-from .value_locate import value_locate
--- gm/plotting/__init__.py
+++
@@ -1,5 +0,0 @@
-#  Copyright (c) 2021, Greg Michael
-#  Licensed under BSD 3-Clause License. See LICENSE.txt for details.
-
-from .ticks import ticks
-from .mpl_check_font import mpl_check_font
