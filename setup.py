2121fae0e391902940fb768b5dec5f85e4a754f5
Andrew Annex <andrew.annex@gmail.com>
2021-09-14 16:23:19 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+with open(here / "README.md") as readme_file:
+    readme = readme_file.read()
+
+with open(here / 'requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    author="Greg Michael",
+    author_email="ggmichael@users.noreply.github.com",
+    python_requires=">=3.6, <4",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="Craterstats - a tool to analyse and plot crater count data for planetary surface dating",
+    entry_points={
+        "console_scripts": [
+            'craterstats=craterstats.cli:main',
+        ],
+    },
+    license="BSD license",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    keywords=["craters"],
+    name="craterstats",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    test_suite="tests",
+    tests_require=required,
+    url="https://github.com/ggmichael/craterstats",
+    version="0.1.0",
+)
