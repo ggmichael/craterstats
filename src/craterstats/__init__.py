0e3da2ec285bd91be2602ac85e5b7e582355b27e
Greg Michael <ggmichael@users.noreply.github.com>
2023-10-30 11:10:54 +0800

    "Bumping version to 3.0.21"

--- src/craterstats/__init__.py
+++ src/craterstats/__init__.py
@@ -1,7 +1,7 @@
 #  Copyright (c) 2021, Greg Michael
 #  Licensed under BSD 3-Clause License. See LICENSE.txt for details.
 
-__version__ = "3.0.20"
+__version__ = "3.0.21"
 
 from .Chronologyfn import Chronologyfn
 from .Productionfn import Productionfn
