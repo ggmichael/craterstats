042aeb0f968cbdd1233de21fa4969b13db73a457
Greg Michael <75208810+ggmichael@users.noreply.github.com>
2025-05-29 16:37:11 +0800

    add gallery

--- make_gallery.py
+++ make_gallery.py
@@ -15,7 +15,13 @@ def main():
     n = None #[5,6,7,24]
     n=cli.demo(n)
 
-    s=['','# Gallery','']
+    s=['''
+# Gallery
+
+This gallery shows the types of plots produced by Craterstats-III, and the command options used to produce them.
+These may be typed on single line following the command `craterstats`, or saved in a file with `.cs` extension
+and run with the command `craterstats -i <filename>.cs`.
+''']
 
     for i in n:
         fn = '{:02d}-demo'.format(i)
