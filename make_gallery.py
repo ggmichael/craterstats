d40a11e62849813620ed10d7c223b04129e821cb
Greg Michael <75208810+ggmichael@users.noreply.github.com>
2025-05-29 14:06:09 +0800

    add gallery

--- make_gallery.py
+++ make_gallery.py
@@ -2,17 +2,13 @@
 
 import craterstats.cli as cli
 import craterstats.gm as gm
-import os
+from pathlib import Path
 import glob
-
-pngs = glob.glob("demo_*.png")
 
 
 def main():
-    d = r'D:\mydocs\code\pycharm\craterstats\docs'
-    os.chdir(d)
-    n = range(0,26) #None #[5,6,7,24]
-    # n=cli.demo(n)
+    n = None #[5,6,7,24] range(0,26) #
+    n=cli.demo(n)
 
     s=['','# Gallery','']
 
@@ -20,12 +16,13 @@ def main():
         fn = '{:02d}-demo'.format(i)
         cs = gm.read_textfile('demo/'+fn+'.cs')
         im = glob.glob("demo/"+fn+"*.p*")
+        im = [Path(p).as_posix() for p in im]
         lnk = ['!['+fn+']('+e+')' if gm.filename(e,'e')=='.png' else f'[View the PDF]({e})' for e in im]
         s+=[*lnk,
             f'\nDemo {i}\n',
             "```", *cs[1:], "```\n"]
 
-    gm.write_textfile(d+r'\gallery.md',s)
+    gm.write_textfile(r'docs/gallery.md',s)
 
 
 if __name__ == '__main__':
