7e4c633db1ce611c6de138f3a3fd2a01a5151133
Greg Michael <75208810+ggmichael@users.noreply.github.com>
2025-06-18 18:06:23 +0800

    fix maths font

--- src/craterstats/Craterplotset.py
+++ src/craterstats/Craterplotset.py
@@ -89,21 +89,38 @@ class Craterplotset:
 
         plt.style.use(('default', 'dark_background')[self.invert])
 
-        # set up font and scaled font size
+        # Path to your local font directory
         font_dir = os.path.join(os.path.dirname(__file__), 'fonts')
-        font_files = [os.path.join(font_dir, f) for f in os.listdir(font_dir) if f.endswith('.ttf')]
 
-        for font_file in font_files:
-            fm.fontManager.addfont(font_file)
+        # Register each font manually
+        for fname in os.listdir(font_dir):
+            if fname.endswith(".ttf"):
+                path = os.path.join(font_dir, fname)
+                fm.fontManager.addfont(path)
+
+        # Use the font (assuming "Noto Sans" is one of the loaded fonts)
+        available_font = 'Noto Sans'
+        #plt.rcParams['font.family'] = available_font
+
+        # Set mathtext font set and fallback
+        plt.rc('mathtext', fontset='custom',
+               rm=available_font,
+               bf=available_font,
+               it=available_font,
+               cal=available_font)
+
+        plt.rcParams['mathtext.fallback'] = 'stixsans'
+
+        # available = sorted({f.name for f in fm.fontManager.ttflist if 'Noto' in f.name})
+        # print("Loaded Noto fonts:", available)
+
         # if os.getenv("CI", "").lower() == "true":
         #     plt.rcParams['font.family'] = 'Noto Sans' #need to rebuild font cache for github action
-        plt.rcParams['font.family'] = 'Noto Sans'
+        #plt.rcParams['font.family'] = 'Noto Sans'
 
         #desired_font = ['Noto Sans','Myriad Pro', 'Verdana', 'DejaVu Sans', 'Tahoma']  # in order of preference
         #Noto Sans now default, but leave selection code in case need to change
         #available_font = gm.mpl_check_font(desired_font)
-        available_font = 'Noto Sans'
-
 
         scale_factor = {'Noto Sans':.89,
                         'Myriad Pro': 1.,
@@ -112,12 +129,22 @@ class Craterplotset:
                        'Tahoma': .93,
                        }[available_font]
         self.scaled_pt_size = self.pt_size * scale_factor
-
-        maths_font = 'DejaVu Sans'
-
         plt.rc('font', family=available_font, size=self.scaled_pt_size)
+
+        # import matplotlib
+        # print(sorted([k for k in matplotlib.rcParams.keys() if 'mathtext' in k]))
+
+        #maths_font = gm.mpl_check_font(['Myriad Pro','Verdana','DejaVu Sans'])
+
         #allow maths to fall-back automatically?
         #plt.rc('mathtext', fontset='custom', rm=maths_font, bf=maths_font + ':bold', cal=maths_font + ':italic')
+        # maths_font = 'Noto Sans'
+        # plt.rc('mathtext', fontset='custom',
+        #        rm=maths_font,
+        #        bf=maths_font+' Bold',
+        #        it=maths_font+' Italic',
+        #        cal=maths_font+' Italic')
+        # plt.rcParams['mathtext.fallback'] = 'cm'
 
         #plt.rcParams['svg.fonttype'] = 'none' # makes text editable, but then not portable
         plt.rcParams['image.composite_image'] = False # prevent optimisation of svg raster
