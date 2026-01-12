c39aee89f63d30278edcf014933910857e1eab74
Greg Michael <75208810+ggmichael@users.noreply.github.com>
2026-01-12 17:53:52 +0800

    add file append

--- src/craterstats/gm/file/write_textfile.py
+++ src/craterstats/gm/file/write_textfile.py
@@ -1,7 +1,7 @@
 #  Copyright (c) 2021-2025, Greg Michael
 #  Licensed under BSD 3-Clause License. See LICENSE.txt for details.
 
-def write_textfile(filename,s,BOM=False):
+def write_textfile(filename,s,BOM=False,append=False):
     '''
     write string or list of strings to text file
 
@@ -9,6 +9,7 @@ def write_textfile(filename,s,BOM=False):
     :param s: string or list of strings
     :return: none
     '''
-    
-    with open(filename, 'w', encoding='utf-8-sig' if BOM else 'utf-8') as file:
+
+    filemode = 'a' if append else 'w'
+    with open(filename, filemode, encoding='utf-8-sig' if BOM else 'utf-8') as file:
         file.writelines('\n'.join(s) if type(s) is list else s)
