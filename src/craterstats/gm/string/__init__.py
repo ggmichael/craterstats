10b17fc23496bc24debd3adaac0def5dcfe4fc13
Greg Michael <75208810+ggmichael@users.noreply.github.com>
2026-06-04 11:38:06 +0800

    tidy up

--- src/craterstats/gm/string/__init__.py
+++ src/craterstats/gm/string/__init__.py
@@ -2,9 +2,11 @@
 #  Licensed under BSD 3-Clause License. See LICENSE.txt for details.
 
 from .bright import bright
+#from .decode_abbreviation import decode_abbreviation
+from .diameter_range import diameter_range
+from .percentile_sigfigs import percentile_sigfigs
+from .quoted_split import quoted_split
 from .scientific_notation import scientific_notation
 from .sigfigs import sigfigs
 from .strip_quotes import strip_quotes
-from .quoted_split import quoted_split
-from .diameter_range import diameter_range
-from .percentile_sigfigs import percentile_sigfigs
+
